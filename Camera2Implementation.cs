7bc1e6d08346a865f6167d84d01704ff03277599
golot <golotuks@gmail.com>
2024-12-31 12:44:15 -0600

    все так же

--- Camera2Implementation.cs
+++ Camera2Implementation.cs
@@ -239,8 +239,12 @@ namespace MauiCameraViewSample.Platforms.Android
             //}  
             // Настройка аргументов для FFmpeg
             //string streamUrl = "rtmp://your-stream-url";
+            //string ffmpegArgs = $"-f rawvideo -pix_fmt yuv420p -s 1920x1080 -r 30 -i pipe:0 " +
+            //                    $"-vcodec libx264 -preset veryfast -b:v 1000k -maxrate 1000k -bufsize 2000k  -acodec aac -b:a 128k -f flv {streamUrl}";
             string ffmpegArgs = $"-f rawvideo -pix_fmt yuv420p -s 1920x1080 -r 30 -i pipe:0 " +
-                                $"-vcodec libx264 -preset veryfast -b:v 1000k -maxrate 1000k -bufsize 2000k  -acodec aac -b:a 128k -f flv -loglevel debug {streamUrl}";
+                    $"-vcodec libx264 -preset veryfast -b:v 1000k -maxrate 1000k -bufsize 2000k " +
+                    $"-acodec aac -b:a 128k -f flv {streamUrl}";
+
             //string ffmpegArgs = $"-f lavfi -i testsrc=size=1920x1080:rate=30 " +
             //  $" -vcodec libx264 -b:v 1000k -maxrate 1000k -bufsize 2000k -acodec aac -b:a 128k -f flv {streamUrl}";
             try
